6ee49fcd3d4e94debbb0bbb64a4578648a80980a
Pooya Kabiri <pooya_kabiri@yahoo.com>
2021-01-08 00:03:50 +0330

    Add Database models

--- main.py
+++ main.py
@@ -3,29 +3,45 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit, send
 
-app = Flask(__name__, template_folder="/Users/alireza/Desktop/Code/991/Internet Engineering/HW4/HW4/template")
+app = Flask(__name__, template_folder="/template")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'true'
 app.config['SECRET_KEY'] = 'guessmeifyoucan'
 socketio = SocketIO(app, cors_allowed_origins="*")
 db = SQLAlchemy(app)
-CORS(app)
 
 
-class products(db.Model):
+class Product(db.Model):
     id = db.Column('product_id', db.Integer, primary_key=True)
-    name = db.Column(db.Integer)
+    name = db.Column(db.String(50))
     price = db.Column(db.Float)
     quantity = db.Column(db.Integer)
 
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+    # def __init__(self, name, price, quantity):
+    #     self.name = name
+    #     self.price = price
+    #     self.quantity = quantity
 
+    @property
+    def serialize(self):
+       """Return object data in easily serializable format"""
+       return {
+           'id'         : self.id,
+           'name'       : self.name,
+           'price'      : self.price,
+           'quantity'   : self.quantity
+       }
 
 db.create_all()
 
+p1 = Product(name='dildo', price=98.1, quantity=40)
+p2 = Product(name='butt plug', price=128.6, quantity=25)
+p3 = Product(name='clip', price=13.8, quantity=164)
+
+db.session.add(p1)
+db.session.add(p2)
+db.session.add(p3)
+db.session.commit()
 
 @app.route('/')
 def show_all():
